b47c1474ba218d42e1884e79ee1928dd6e40b521
murilo.moreira <xxmuriloxx0077@gmail.com>
2026-01-28 13:43:33 -0300

    Corrige server.js removendo marcador de conflito e ajustando sintaxe

--- src/server.js
+++ src/server.js
@@ -18,10 +18,8 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Acesse essa API em http://localhost:8000")
-=======
-  console.log("Boas vindas a API do All Books")
-  console.log("API disponível na URL http://localhost:8000")
->>>>>>> 72cef01 (Altera o server.js)
+    console.log("Acesse essa API em http://localhost:8000")
 })
+
+console.log("Boas vindas a API do All Books")
+console.log("API disponível na URL http://localhost:8000")
